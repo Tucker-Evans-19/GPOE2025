350453bb8aa37af332e61fa24246f17c9374aac4
jlball <jlball813@gmail.com>
2024-09-08 15:55:30 -0400

    Fixed specification of run directory, removed microseconds from timestamp used in saved files

--- timelapse.py
+++ timelapse.py
@@ -11,19 +11,20 @@ parser = argparse.ArgumentParser(prog="GPOE2025 All Sky Timelapse",
     description = "Takes exposures at a fixed interval for a timelapse, timestamps them, and stores them as numpy binary files")
 
 parser.add_argument("-c", "--config", help="Name of JSON-formatted configuration file to use", type=str)
-
 args = parser.parse_args()
 
 # Load configuration file
 with open(args.config, "rb") as file:
     config_dict = json.load(file)
 
-# Create working directory
+# Create and enter working directory
 if capture_config["run_id"] == "iso_date"
-    run_dir = 
-    os.mkdir
+    run_dir = datetime.now().isoformat()[:9] # look for more elegant solution?
 else:
-    np.save(capture_config["run_id"], array)
+    run_dir = capture_config["run_id"]
+
+os.mkdir(run_dir)
+os.chdir(run_dir)
 
 # Setup Camera
 cam = Picamera2()
@@ -40,16 +41,20 @@ for i in range(0, config_dict["num_frames"]):
     init_time = time.perf_counter()
     end_time = datetime.now() + timedelta(seconds=config_dict["capture_interval"])
 
-    array = cam.capture_array("main")
-    array = array[:, 250:-480, :]
-    print(f"Captured Frame {i}")
+    timestamp = datetime.now()
+    image_arr = cam.capture_array("main")
 
-    if capture_config["run_id"] == "iso_date"
-        np.save(datetime.now().isoformat(), array)
-    else:
-        np.save(capture_config["run_id"], array)
+    image_arr = image_arr[:, 250:-480, :] # Cuts off all black regions of image and reduces data size by ~18 %
+    np.save(timestamp.replace(microsecond=0).isoformat(), image_arr)
 
+    if datetime.now() >= end_time:
+        print("WARNING: Execution of frame capture code exceeded specified capture interval. Consider reducing exposure time or increasing the invterval.")
+
+    # Ensures spacing between frames is even regardless of code execution time above
     while datetime.now() < end_time:
         continue        
     
+    print(f"Captured Frame {i}")
     print(f"Time elapsed: {time.perf_counter() - init_time}")
+
+print("Timelapse Completed!")
