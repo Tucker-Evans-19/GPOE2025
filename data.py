f584a9e8bd03a2d479c65c2361f1c917427b646b
Noah Wolfe <newolfe@mit.edu>
2024-09-15 17:04:44 -0400

    scaffold of main event loop for all devices

---
+++ data.py
@@ -0,0 +1,69 @@
+import h5py
+import numpy as np
+
+
+def _create_file(path, dataset_parameters):
+    """
+    path: must NOT have h5py on the end
+    n_expected: int, the number of integers expected
+    """
+    
+    # TODO: warnings in case the time fucks up and it drives to overwrite an
+    # existing file... 
+
+    # or maybe just have it append a 'v2' to the path
+
+    f = h5py.File(path, 'w')
+
+    for params in dataset_parameters:
+        f.create_dataset(**params)
+
+    return path
+
+
+def _create_files(outdir, name, n_measurements, n_exposures, n_xpix, n_ypix, n_colors):
+    exposure_dataset_parameters = [
+        dict(name='timestamp', shape=(n_exposures,), dtype=np.float64),
+        dict(
+            name='exposure',
+            shape=(n_exposures, n_xpix, n_ypix, n_colors),
+            dtype=np.float64
+        )
+    ]
+
+    measurement_dataset_parameters = [
+        dict(name='timestamp', shape=(n_measurements,), dtype=np.float64),
+        dict(name='temperature', shape=(n_measurements,), dtype=np.float32),
+        dict(name='magnetic_field', shape=(n_measurements, 3), dtype=np.float32),
+    ]
+
+    exposure_file_path = _create_file(
+        f'{outdir}/{name}-exposures.hdf5',
+        exposure_dataset_parameters
+    )
+
+    measurement_file_path = _create_file(
+        f'{outdir}/{name}-measurements.hdf5',
+        measurement_dataset_parameters
+    )
+
+
+def insert_datum(path, datum, index):
+    with h5py.File(path, 'r') as f:
+        for key, value in datum.items():
+            f[key][index] = value
+
+
+"""
+example program loop:
+
+path = create_file(...)
+
+# measurements
+for i in range(...):
+    temperature = get_temperature(...)
+    bx, by, bz = get_magnetic_filed(...)
+    ...
+
+"""
+
---
+++ main.py
@@ -0,0 +1,134 @@
+import os
+import asyncio
+from datetime import datetime, timezone
+
+import numpy as np
+
+from data import _create_files
+from data import insert_datum
+
+SECONDS_TO_MINUTES = 1 / 60
+SECONDS_TO_HOURS = SECONDS_TO_MINUTES / 60
+SECONDS_TO_DAYS = SECONDS_TO_HOURS / 24
+
+excess_minutes = 10
+exposure_cadence = 1 / 15 # [exposures per second]
+measurement_cadence = 1 # [measurements per second]
+
+n_exposures = int(
+    exposure_cadence / SECONDS_TO_HOURS
+    + excess_minutes * exposure_cadence / SECONDS_TO_MINUTES
+)
+n_measurements = int(
+    measurement_cadence / SECONDS_TO_HOURS
+    + excess_minutes * measurement_cadence / SECONDS_TO_MINUTES
+)
+
+n_xpix = 100
+n_ypix = 100
+
+
+def get_now():
+    """ Get's the current UTC time """
+    now = datetime.now(timezone.utc)
+    return now
+
+
+def get_datestr(datetime):
+    return datetime.isoformat().split('T')[0]
+
+
+create_files = lambda outdir, name: _create_files(
+    outdir, name, n_measurements, n_exposures, n_xpix, n_ypix
+)
+
+
+async def get_measurements():
+    await asyncio.sleep(1)
+    return dict(
+        temperature=np.random.normal(loc=0, scale=1),
+        magnetic_field=np.random.normal(loc=0, scale=1, shape=(3,))
+    )
+
+
+async def get_exposure():
+    await asyncio.sleep(30)
+    return dict(
+        exposure=np.random.normal(loc=0, scale=1, shape=(n_xpix, n_ypix, 3)),
+    )
+
+
+async def main(): 
+    start = get_now()
+    start_of_day_timestamp = start.timestamp()
+    start_of_hour_timestamp = start.timestamp()
+
+    outdir = f'./{get_datestr(start)}'
+    os.makedirs(outdir, exist_ok=True)
+
+    exposure_file_path, measurement_file_path = create_files(
+        f'{outdir}/{start.hour}.hdf5'
+    )
+
+    exposure_index = 0
+    measurement_index = 0
+
+    while ...:
+        current = get_now()
+        current_timestamp = current.timestamp()
+
+        if (current_timestamp - start_of_day_timestamp) * SECONDS_TO_DAYS > 1:
+            start_of_day_timestamp = current_timestamp
+            start_of_hour_timestamp = current_timestamp
+
+            outdir = f'./{get_datestr(current)}'
+            exposure_file_path, measurement_file_path = create_files(
+                f'{outdir}/{start.hour}.hdf5'
+            )
+
+            exposure_index = 0
+            measurement_index = 0
+
+        elif (current_timestamp - start_of_hour_timestamp) * SECONDS_TO_HOURS > 1:
+            start_of_hour_timestamp = current_timestamp
+
+            exposure_file_path, measurement_file_path = create_files(
+                f'{outdir}/{start.hour}.hdf5'
+            )
+
+            exposure_index = 0
+            measurement_index = 0
+
+        # camera 
+        # kick off an exposure...
+        exposure_start_timestamp = get_now().timestamp()
+        exposure_task = asyncio.create_task(get_exposure())
+
+        try:
+            while not exposure_task.done():
+                measurement_timestamp = get_now().timestamp()
+                measurements = await get_measurements()
+                measurements = dict(
+                    timestamp=measurement_timestamp,
+                    **measurements
+                )
+                insert_datum(
+                    measurement_file_path, measurements, measurement_index
+                )
+                measurement_index += 1
+        except asyncio.CancelledError:
+            print("Fast operations cancelled.")
+        finally:
+            exposure = await exposure_task
+            exposure_datum = dict(
+                timetamp=exposure_start_timestamp,
+                exposure=exposure
+            )
+            insert_datum(
+                exposure_file_path, exposure_datum, exposure_index
+            )
+            exposure_index += 1
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
