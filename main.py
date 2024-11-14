69f3ee10aafafca50f6b76bb75066cf232d79ac9
Noah Wolfe <newolfe@mit.edu>
2024-11-14 17:08:55 -0500

    big cleanup of main.py

--- main.py
+++ main.py
@@ -78,52 +78,156 @@ cam = None
 therm_device_file = None
 
 
-async def insert_in_hdf5(path, datum, index):
+async def insert_datum_async(path, datum, index):
+    """ async wrapper around `insert_datum` """
     loop = asyncio.get_running_loop()    
+    await loop.run_in_executor(None, insert_datum, path, datum, index)
 
+async def insert_in_hdf5(path, datum, index):
+    """ wraps `insert_datum_async` with error handling. Note, we take a fixed
+        timeout for all writes of 2 seconds, as the largest items we'll write
+        are the exposures which are ~6 MB, and write speed on the pi/usb
+        seem to be of order >~ 10 MB/s    
+    """
     try:
-        await loop.run_in_executor(None, insert_datum, path, datum, index)
+        await asyncio.wait_for(
+            insert_datum_async(path, datum, index),
+            timeout=2
+        )
+    except asyncio.TimeoutError:
+        log.error(f'timeout when writing to {path} at index {index}')
     except Exception as e:
         log.error(e)
 
 
+async def get_temperature_async():
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(
+        None,
+        get_temperature,
+        therm_device_file
+    )
+
+
+async def get_magnetometer_measurement_async():
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(
+        None,
+        get_magnetometer_measurement,
+        rm
+    )
+
+
 async def get_measurements():
+    loop = asyncio.get_running_loop()
+
+    timestamp = get_now().timestamp()
+
     try:
-        temperature = get_temperature(therm_device_file)
+        temperature = await asyncio.wait_for(
+            get_temperature_async(),
+            timeout=1       
+        )
+    except asyncio.TimeoutError:
+        temperature = 0
+        log.error('thermometer timeout')
     except Exception as e:
         temperature = 0
         log.error(e)
 
     try:
-        magnetic_field = get_magnetometer_measurement(rm)
+        magnetic_field = await asyncio.wait_for(
+            get_magnetometer_measurement_async(),
+            timeout=5 * rm.measurement_time
+        )
+    except asyncio.TimeoutError:
+        magnetic_field = np.zeros((3,))
+        log.error('magnetometer timeout')
     except Exception as e:
         magnetic_field = np.zeros((3,))
         log.error(e) 
 
+    log.debug(
+        'measured temperature & magnetic field: '
+        f'temp = {temperature}, '
+        f'(Bx, By, Bz) = {magnetic_field}'
+    )
+
     return dict(
+        timestamp=timestamp,
         temperature=temperature,
         magnetic_field=magnetic_field
     )
 
 
+async def get_and_write_measurements(path, index, event=None):
+    """
+    Take measurements & write them to an hdf5 file. Note that even if the
+    measurement process or write fails, it will still increment the index;
+    this allows us to compute statistics for failed measurements/writes later.
+
+    path: str, path to hdf5 file to write
+    index: int, index in the appropriate dataset(s) to write
+    event: asyncio.Event, optional event to wait for
+    """
+
+    datum = await get_measurements()
+    await insert_in_hdf5(path, datum, index)
+
+    if event is not None:
+        await event.wait()
+ 
+    return index + 1 
+
+
+async def take_single_exposure_async():
+    """ async wrapper around `take_single_exposure` """
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, take_single_exposure, cam)
+
+
 async def get_exposure():
-    loop = asyncio.get_running_loop()   
+    timestamp = get_now().timestamp()
 
     if cam is None:
         # if there's no camera, there's no wait for an exposure.
         # to make sure the timing doesn't get messed up, wait
         # that long anyway.
+        # I want to replace this with asyncio.Event synchronization at some
+        # point b/c it's cleaner.
         await asyncio.sleep(exposure_time)
 
-    image_arr = await loop.run_in_executor(None, take_single_exposure, cam)    
+    try:
+        image_arr = await asyncio.wait_for(
+            take_single_exposure_async(),
+            timeout=exposure_timeout
+        )
+    except asyncio.TimeoutError:
+        log.error('exposure timeout')
+        image_arr = np.zeros((n_xpix, n_ypix, n_colors), dtype=np.uint8)
+    except Exception as e:
+        log.error(e)
+        image_arr = np.zeros((n_xpix, n_ypix, n_colors), dtype=np.uint8)
 
     return dict(
+        timestamp=timestamp,
         exposure=image_arr
     )
 
 
+async def get_and_write_exposure(path, index, event=None):
+    datum = await get_exposure()
+    await insert_in_hdf5(path, datum, index)
+    
+    if event is not None:
+        await event.wait()
+
+    return index + 1
+
 
 async def main():
+    loop = asyncio.get_event_loop()
+
     start = get_now()
     start_of_day_timestamp = start.timestamp()
     start_of_hour_timestamp = start.timestamp()
@@ -142,7 +246,8 @@ async def main():
         current = get_now()
         current_timestamp = current.timestamp()
         target_end_timestamp = current_timestamp + 1 / exposure_cadence
-
+        
+        # TODO: move all of this into it's own function for clarity
         if (current_timestamp - start_of_day_timestamp) * SECONDS_TO_DAYS > 1:
             start_of_day_timestamp = current_timestamp
             start_of_hour_timestamp = current_timestamp
@@ -170,80 +275,23 @@ async def main():
             exposure_index = 0
             measurement_index = 0
 
-        exposure_start_timestamp = get_now().timestamp()
-        exposure_task = asyncio.create_task(get_exposure())
+        exposure_task = asyncio.create_task(get_and_write_exposure(
+            exposure_file_path,
+            exposure_index
+        ))
 
         try:
             while not exposure_task.done():
-                measurement_timestamp = get_now().timestamp()
-
-                try:
-                    measurements = await asyncio.wait_for(
-                        get_measurements(),
-                        timeout=measurement_timeout
-                    )
-                except asyncio.TimeoutError:
-                    log.warning(f'thermometer or magnetometer timeout')
-                    measurements = dict(
-                        temperature=0,
-                        magnetic_field=np.zeros(3)
-                    )
-    
-                log.debug(
-                    'measured temperature & magnetic field: '
-                    f'temp = {measurements["temperature"]}, '
-                    f'(Bx, By, Bz) = {measurements["magnetic_field"]}'
+                measurement_index = await get_and_write_measurements(
+                    measurement_file_path,
+                    measurement_index
                 )
-
-                measurement_datum = dict(
-                    timestamp=measurement_timestamp,
-                    **measurements
-                )
-                
-                try:
-                    await asyncio.wait_for(
-                        insert_in_hdf5(
-                            measurement_file_path,
-                            measurement_datum,
-                            measurement_index
-                        ),
-                        timeout=1
-                    )
-                except asyncio.TimeoutError:
-                    log.error(f'writing measurements timeout')
-
-                measurement_index += 1
         except asyncio.CancelledError:
             log.info('Temperature/magnetic field measurements cancelled.')
         finally:
-            try:
-                exposure = await asyncio.wait_for(
-                    exposure_task,
-                    timeout=exposure_timeout
-                )
-            except asyncio.TimeoutError:
-                log.warning(f'timeout occured at {exposure_start_timestamp} when calling camera')
-                exposure = dict(exposure=np.zeros(n_xpix, n_ypix, n_colors, dtype=np.uint8))
- 
-            exposure_datum = dict(
-                timestamp=exposure_start_timestamp,
-                **exposure
-            )
+            await exposure_task
+            exposure_index = exposure_task.result() 
 
-            try:
-                await asyncio.wait_for(
-                    insert_in_hdf5(
-                        exposure_file_path,
-                        exposure_datum,
-                        exposure_index
-                    ),
-                    timeout=10
-                )
-            except asyncio.TimeoutError:
-                log.warning('writing exposure timeout')
-
-            exposure_index += 1
- 
         # once all of the above stuff is done, wait some delta # of seconds
         approx_sleep_length = target_end_timestamp - get_now().timestamp() 
         log.debug(
@@ -291,12 +339,19 @@ if __name__ == '__main__':
             log.warning(e)
 
     try:
+        #image_arr = asyncio.run(get_exposure())
+        from time import time
+        t1 = time()
         image_arr = take_single_exposure(cam)
+        t2 = time()
+        print(f'op took {t2 - t1} s')
         n_xpix, n_ypix, n_colors = image_arr.shape
         log.info('captured test exposure')
         log.debug(
             f'dims (n_xpix, n_ypix, n_colors) = {n_xpix, n_ypix, n_colors}'
         )
+        log.debug(f'exposure looks like: {image_arr}')
+        quit()
     except Exception as e:
         if camera_critical:
             log.error(e)
