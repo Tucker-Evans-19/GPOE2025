6ba1afdc70c9208a8e84b9eb285532f8f766b411
John Ball <jlball@mit.edu>
2025-01-20 14:19:19 -0900

    Updated run.sh to be much simpler and not rely on the existence of another file to run

--- run.sh
+++ run.sh
@@ -1,10 +1,6 @@
 # !/bin/bash
 
 # check if a special file exists
-if [ -f /home/gpoe/GPOE2025/RUN ]; then
-    echo 'found RUN file; starting main control loop!'
-    echo "Using config file:" $1
-    cd /home/gpoe/GPOE2025
-    rm RUN
-    nohup python -u main.py -c $1 > run.log 2> run.err &
-fi
+source /home/gpoe/.venv/bin/activate
+echo $(which python) > /home/gpoe/python.log
+python -u /home/gpoe/GPOE2025/main.py -c $1 > /home/gpoe/script.log
